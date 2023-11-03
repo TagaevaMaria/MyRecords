a660daa88138cdf9b8ec878c0fe15b85c58dd02b
vcebu <vcebudet5.ok@mail.ru>
2023-11-03 22:54:56 +0700

    dev screenPhoyto

--- lib/screens/photo/screen_photo.dart
+++ lib/screens/photo/screen_photo.dart
@@ -23,43 +23,24 @@ class ScreenFoto extends StatelessWidget {
     final _model = context.watch<ScreenFotoModel>();
 
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Фото"),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _model.showPicker(context),
+        child: const Icon(Icons.add_a_photo),
+      ),
+      appBar: AppBar(
+        title: const Text("Фото"),
+      ),
+      body: GridView.builder(
+        itemCount: _model.listImages.length,
+        itemBuilder: (BuildContext context, int index) {
+          return _model.image != null
+              ? Image.file(_model.image!)
+              : Text('l;mjnjkn');
+        },
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
         ),
-        body: Column(
-          children: [
-            Column(
-              children: [
-                Center(
-                  child: GestureDetector(
-                    onTap: () => _model.showPicker(context),
-                    child: Container(
-                      width: 300,
-                      height: 50,
-                      color: Colors.blue,
-                      child: const Center(child: Text('Выбрать изображение')),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 30),
-                Container(
-                  width: 300,
-                  height: 300,
-                  color: Colors.blue,
-                  child: _model.selectedImgae != null
-                      ? Image.file(
-                          _model.selectedImgae!,
-                          fit: BoxFit.fill,
-                        )
-                      : const Icon(Icons.add_a_photo),
-                )
-              ],
-            )
-          ],
-        ));
+      ),
+    );
   }
 }
-
-/// если после закрытия диалогового окна хочу передать результат, то делаем так Navigator.pop(context, result);
-///GestureDetector(
-//               onTap: () => _model.showPicker(context),
--- lib/screens/photo/screen_photo_model.dart
+++ lib/screens/photo/screen_photo_model.dart
@@ -6,23 +6,28 @@ import 'package:image_picker/image_picker.dart';
 class ScreenFotoModel extends ChangeNotifier {
   ///Метод,который выбирает изображение из галлереи.
 
-  File? selectedImgae;
-  ImagePicker image = ImagePicker();
+  File? image;
+  final picker = ImagePicker();
+
+  final List<XFile> listImages = [];
 
   Future pickImageFromGallery() async {
-    final returnedImage =
-        await ImagePicker().pickImage(source: ImageSource.gallery);
-    if (returnedImage == null) return;
-    selectedImgae = File(returnedImage.path);
+    final image = await ImagePicker().pickImage(source: ImageSource.gallery);
+    if (image == null) return;
+    final imageTemporary = File(image.path);
+    this.image = imageTemporary;
+    final listImagesGallery = listImages.add(image);
+
     notifyListeners();
   }
 
   ///Метод,который выбирает изображение из камеры.
   Future pickImageFromCamera() async {
-    final returnedImage =
-        await ImagePicker().pickImage(source: ImageSource.camera);
-    if (returnedImage == null) return;
-    selectedImgae = File(returnedImage.path);
+    final image = await ImagePicker().pickImage(source: ImageSource.camera);
+    if (image == null) return;
+    final imageTemporary = File(image.path);
+    this.image = imageTemporary;
+    final listImagesCamera = listImages.add(image);
     notifyListeners();
   }
 
@@ -54,24 +59,4 @@ class ScreenFotoModel extends ChangeNotifier {
           );
         });
   }
-
-  ///Блок по отображению фото с камера или с галлерии
-
-  List<MyImages> myImages = [
-    MyImages(
-      Image.asset('assets/images/photo1.jpg'),
-    ),
-    MyImages(
-      Image.asset('assets/images/photo2.jpeg'),
-    ),
-    MyImages(
-      Image.asset('assets/images/photo3.jpeg'),
-    ),
-  ];
-}
-
-class MyImages {
-  final Image myPhoto;
-
-  MyImages(this.myPhoto);
 }
