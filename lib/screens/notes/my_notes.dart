d8d0f631b3c36e1529be6caa8e28c7d0eae075db
Marija Tagaeva <108568161+TagaevaMaria@users.noreply.github.com>
2024-01-07 15:54:01 +0700

    Обучение Git. Создание новой ветки для каждого нового задания.

--- lib/screens/notes/my_notes.dart
+++ lib/screens/notes/my_notes.dart
@@ -5,6 +5,6 @@ class MyNotes extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return  Scaffold(body: Text('НЕ ПОЛУЧАЕТСЯ В ГИТХАБЕ УВИДЕТЬ ПУШ'));
   }
 }
